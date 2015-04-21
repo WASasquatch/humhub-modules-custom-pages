245f1e29153972c852f6b625a684a977c86258fd
Jordan Thompson <jordanslost@gmail.com>
2015-04-21 09:18:30 -0700

    Update m150413_030602_create_visibility_column.php

--- migrations/m150413_030602_create_visibility_column.php
+++ migrations/m150413_030602_create_visibility_column.php
@@ -4,7 +4,7 @@ class m150413_030602_create_visibility_column extends EDbMigration
 {
 	public function up()
 	{
-		$this->createColumn('custom_pages_page', 'visibility', 'int(1)');
+		$this->addColumn('custom_pages_page', 'visibility', 'int(1)');
 	}
 
 	public function down()
