e5783c14969df985394f1a3c9347e8cf85099c6c
Jordan Thompson <jordanslost@gmail.com>
2015-05-12 13:22:40 -0700

    Update CustomStackWidget.php

--- widgets/CustomStackWidget.php
+++ widgets/CustomStackWidget.php
@@ -27,8 +27,8 @@ class CustomStackWidget extends HWidget {
         if (!isset($this->notemplate)) {
             $this->notemplate = 0;
         }
-        if (Yii::app()->request->getParam('sguid') != '' && ($this->space = Space::model()->findByPk(Yii::app()->request->getParam('sguid'))) != null) {
-            if (!isset($this->space->id)) {
+        if (Yii::app()->request->getParam('sguid') != '' && $this->space = Space::model()->findByPk(Yii::app()->request->getParam('sguid'))) {
+            if ($this->space->id != '') {
                 $this->space = false;
             }
         }
@@ -36,32 +36,11 @@ class CustomStackWidget extends HWidget {
     }
     
     public function run() {
-        if(Yii::app()->user->isGuest && $this->visibility != CustomPage::VISIBILITY_MEMBER) {
-            if ($this->targets && $this->space) {
-                if (@in_array($this->space->guid, $this->targets) || $this->space->guid == $this->targets) {
-                    if ((int)$this->notemplate == 1) {
-                        $this->render('blankWidget', array(
-                            'id' => $this->id,
-                            'title' => $this->title,
-                            'content' => $this->content,
-                            'icon' => $this->icon,
-                            'user' => Yii::app()->user,
-                            'targets' => $this->targets,
-                            'space' => $this->space,
-                        ));
-                    } else {
-                        $this->render('genericWidget', array(
-                            'id' => $this->id,
-                            'title' => $this->title,
-                            'content' => $this->content,
-                            'icon' => $this->icon,
-                            'user' => Yii::app()->user,
-                            'targets' => $this->targets,
-                            'space' => $this->space,
-                        ));
-                    }
-                }
-            } else {
+        if(Yii::app()->user->isGuest && $this->visibility == CustomPage::VISIBILITY_MEMBER) {
+            continue;
+        }
+        if ($this->targets && $this->space) {
+            if (@in_array($this->space->guid, $this->targets) || $this->space->guid == $this->targets) {
                 if ((int)$this->notemplate == 1) {
                     $this->render('blankWidget', array(
                         'id' => $this->id,
@@ -69,6 +48,7 @@ class CustomStackWidget extends HWidget {
                         'content' => $this->content,
                         'icon' => $this->icon,
                         'user' => Yii::app()->user,
+                        'targets' => $this->targets,
                         'space' => $this->space,
                     ));
                 } else {
@@ -78,9 +58,30 @@ class CustomStackWidget extends HWidget {
                         'content' => $this->content,
                         'icon' => $this->icon,
                         'user' => Yii::app()->user,
+                        'targets' => $this->targets,
                         'space' => $this->space,
                     ));
                 }
+            }
+        } else {
+            if ((int)$this->notemplate == 1) {
+                $this->render('blankWidget', array(
+                    'id' => $this->id,
+                    'title' => $this->title,
+                    'content' => $this->content,
+                    'icon' => $this->icon,
+                    'user' => Yii::app()->user,
+                    'space' => $this->space,
+                ));
+            } else {
+                $this->render('genericWidget', array(
+                    'id' => $this->id,
+                    'title' => $this->title,
+                    'content' => $this->content,
+                    'icon' => $this->icon,
+                    'user' => Yii::app()->user,
+                    'space' => $this->space,
+                ));
             }
         }
     }   
